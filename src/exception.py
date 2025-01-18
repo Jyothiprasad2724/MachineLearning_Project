b59cf23b6549e364ed899ffd05b638733c21dfdc
Jyothiprasad2724 <jyothiprasadkommuru27@gmail.com>
2025-01-18 15:36:25 +0530

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,25 @@
+import sys
+
+
+def error_message_details(error,erro_details:sys):
+    _,_,exc_tb = erro_details.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "error message at python script [{0}] line number [{1}] error message [{2}]".format(
+        file_name,exc_tb.tb_lineno,str(error))
+
+    
+
+    return error_message
+
+
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_details:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_details(error_message,erro_details=error_details)
+
+    def __str__(self):
+        return self.error_message
+    
+
+
